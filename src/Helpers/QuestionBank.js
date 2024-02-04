221e335993a7f325ec7928d1df60567c10531b06
steverayan99 <steverayan123@gmail.com>
2024-02-04 17:39:41 +1100

    first commit

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+        prompt: "What is 2+2?",
+        optionA: "22",
+        optionB: "20",
+        optionC: "18",
+        optionD: "4",
+        answer: "D",
+    },
+    {
+        prompt: "What is 4+4?",
+        optionA: "22",
+        optionB: "8",
+        optionC: "18",
+        optionD: "4",
+        answer: "B",
+    },
+    {
+        prompt: "What is 3+3?",
+        optionA: "22",
+        optionB: "20",
+        optionC: "6",
+        optionD: "4",
+        answer: "C",
+    },
+]
